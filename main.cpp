dc655ab856dcaf962805c55760cfe6d295628290
k2supra <andrijkorolevic@gmail.com>
2023-01-18 22:01:16 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int main() {
+	cout << "Enter a number ->";
+	int num, step;
+	cin >> num;
+	cout << "Enter 2nd number ->";
+	cin >> step;
+	cout << "\n = " << pow(num, step);
+}
